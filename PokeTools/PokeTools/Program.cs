7df75efc3a4da93e94c4a1214d210b91dbd7873d
5argan <tizian.hoffmann@gmail.com>
2016-07-30 14:59:39 +0200

    added logger support

--- PokeTools/PokeTools/Program.cs
+++ PokeTools/PokeTools/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,10 @@ namespace PokeTools
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
+            //log4net.Config.XmlConfigurator.Configure();
+            //ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            //logger.Debug("test");
+            //logger.Info("testtt");
         }
     }
 }
